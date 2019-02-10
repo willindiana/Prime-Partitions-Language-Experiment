95c69bb8b0b853492a3f28ada6a12db1fc8c28fa
luke_brom@taylor.edu <lbrom@cse21706.cs.local>
2019-02-09 23:32:14 -0500

    Finished the ada implementation

--- primeada.adb
+++ primeada.adb
@@ -1,72 +1,89 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Containers.Vectors; use Ada.Containers;
 
 
 procedure primeada is
 
+  package Integer_Vectors is new Vectors(Natural, Integer);
+
 function is_prime (n: Integer) return Boolean is
   Target : Integer := n;
   Result : Boolean := True;
-begin
-  Target := n;
+  begin
+    Target := n;
 
-  Put_Line("Hello, Prime Function!");
+    if Target > 1 then
+      for N in Integer range 2..(Target-1) loop
+        if Target mod N = 0 then
+          return False;
+        end if;
+      end loop;
+    else
+      return False;
+    end if;
 
+    return True;
+  end is_prime;
 
+function primes (a: Integer; b: Integer) return Integer_Vectors.Vector is
+  prime_numbers : Integer_Vectors.Vector;
 
-  if Target > 1 then
-    for N in Integer range 2..(Target-1) loop
-      if Target mod N = 0 then
-      Result := False;
-      exit;
+  begin
+    for N in Integer range a..b loop
+      if is_prime(N) then
+        prime_numbers.Append(N);
       end if;
     end loop;
-    else
-      Result := False;
-  end if;
 
-  return Result;
-end is_prime;
+  return prime_numbers;
+  end primes;
 
-function primes (a, b: Integer) return Vector is
+procedure prime_partitions (n: Integer; k: Integer; lst: out Integer_Vectors.Vector) is
+  p : Integer;
+  begin
+    
+    if n = 0 then
+      for C in lst.Iterate loop
+        Put(Integer_Vectors.Element(C));
+        if Integer_Vectors.Element(C) /= lst.Last_Element then
+          Put(" + ");
+        end if;
+      end loop;
+      new_line(1);
+    end if;
+
+    if n > k then
+      for C in primes(k+1,n).Iterate loop
+        p := Integer_Vectors.Element(C);
+        Integer_Vectors.Append(lst,p);
+        prime_partitions(n - p, p, lst);
+        Integer_Vectors.Delete_Last(lst);
+      end loop;
+    end if;
+
+    return;
+  end prime_partitions;
+
+number : Integer;
+prime_nums : Integer_Vectors.Vector;
+C : Integer_Vectors.Cursor;
 
 begin
-    Put_Line("Primes Function");
 
-end primes;
+  while True loop
+    Put("Enter a number (non-number to quit): ");
 
-function prime_partitions (a, b: Integer) return Vector is
+    begin
+      Get(number);
+    exception
+      when others => 
+        return;
+    end;
 
-begin
-  Put_Line("Prime partitions function");
+    prime_partitions(number, 1, prime_nums);
 
-  retur
-end prime_partitions;
-
-  number : Integer;
-  prime : Boolean;
-begin
-   Ada.Text_IO.Put_Line("Prime Partitions Program");
-   while True loop
-     Put_Line("Enter a number (non-number to quit): ");
-
-     Get(number);
-
-
-     Put(number);
-     new_line(1);
-
-     prime := is_prime(number);
-
-     if prime then
-       Put_Line("Prime Number");
-     -- end if;
-
-     else
-       Put_Line("Not Prime");
-     end if;
-
-   end loop;
+  end loop;
 
 end primeada;
