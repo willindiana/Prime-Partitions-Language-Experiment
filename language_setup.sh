ed5e79469b35b02b46fd3a667acb2d7dbd864a27
WKercher <willindiana@gmail.com>
2019-02-10 22:38:46 -0500

    Added Screenshots, reorganized folder structure, and finished writeup.

---
+++ language_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Programming Languages
+sudo apt install gnat           # Ada
+sudo apt install mono-mcs       # C# (Mono)
+sudo apt install golang-go      # Go
+sudo apt install ghc            # Haskell
+sudo apt install nodejs         # Javascript
+sudo apt install perl           # Perl
+sudo apt install swi-prolog     # Prolog (swipl)
+sudo apt install python3        # Python 3 (not 2!)
+sudo apt install ocaml          # OCaml
+sudo apt install ruby           # Ruby
+sudo apt install rustc          # Rust
+sudo apt install racket         # Scheme
